13ea078a807797b3e3aeff801247cfa3fe957b1e
Shane Rosenthal <srosenthal82@gmail.com>
2025-03-03 14:36:15 -0500

    update

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function(){
+   return redirect()->route('slots', 'default');
+});
 Route::get('/slots/{slotMachine:name}', \App\Livewire\Machine::class)->name('slots');
